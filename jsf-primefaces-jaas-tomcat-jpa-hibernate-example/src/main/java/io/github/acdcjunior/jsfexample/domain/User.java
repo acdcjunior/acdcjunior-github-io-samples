958cdbb9a2238d9135d260007870ede9a0d9eed6
acdcjunior <acdcjunior@gmail.com>
2014-12-09 19:28:31 -0200

    Created new example project from jsf-primefaces-jaas-tomcat-example/ sources

---
+++ jsf-primefaces-jaas-tomcat-jpa-hibernate-example/src/main/java/io/github/acdcjunior/jsfexample/domain/User.java
@@ -0,0 +1,20 @@
+package io.github.acdcjunior.jsfexample.domain;
+
+public class User {
+
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        return this.getUsername();
+    }
+
+}
